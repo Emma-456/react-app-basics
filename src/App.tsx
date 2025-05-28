be61ef4f5758a27c656c3c3709d6c3cd73f7ddf2
Emma-456 <agbajeeema456@gmail.com>
2025-05-28 14:15:40 +0100

    Added Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
-import Message from "./Message";
+
+import ListGroup from "./components/ListGroup";
 
 function App() {
-  return <div><Message /></div>
+  let items = ["The Mentalist", "HIMYM", "Community", "Banshee", "FBI", "Atlanta"]
+
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  }
+  return (
+    <div>
+      <ListGroup items={items} heading="Films" onSelectItem={handleSelectItem}>
+
+      </ListGroup>
+    </div>
+  )
 }
 
 export default App;

421e3237792bc3e76243c675bf88648f9ce600e1
Emma-456 <agbajeeema456@gmail.com>
2025-05-28 14:58:11 +0100

    Initial Commit

---
+++ src/Message.tsx
@@ -0,0 +1,11 @@
+
+// pascal casing
+function Message() {
+    // JSX: JavaScript XML
+    const name = "Ayo";
+    if (name)
+        return <h1>Hello {name}</h1>;
+    return <h1>Hello World</h1>
+}
+
+export default Message;
